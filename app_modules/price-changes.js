1e00cea0556972573db6fdc080b3905b0663ea24
caH40 <kmvsancho@gmail.com>
2022-01-28 10:34:29 +0300

    small corrections

--- app_modules/price-changes.js
+++ app_modules/price-changes.js
@@ -3,18 +3,20 @@ const Product = require('../models/Product');
 async function priceChanges(ctx, username) {
 	try {
 		const productArr = await Product.find({ user: username });
-		productArr.forEach(async product => {
-			let post = '';
+		productArr.forEach(product => {
+			let post = `${product.nameRequest}\n`;
 			for (let index = 0; index < product.prices.length - 1; index++) {
 				if (index + 1 < product.prices.length) {
-					compared = product.prices[index].price - product.prices[index + 1].price;
+					compared = (product.prices[index].price - product.prices[index + 1].price);
 				}
 				const date = new Date(product.prices[index + 1].date).toLocaleString();
 				if (compared !== 0) {
-					post = post + `${date}, ${compared}`;
+					;
+					post = post + `${date}, ${compared < 0 ? compared.toFixed(2) : "+" + compared.toFixed(2)}\n`;
 				}
 			}
-			await ctx.reply(post ? post : `Не было изменений цены:\n<u>${product.nameRequest}</u>`, { parse_mode: 'html' });
+			ctx.reply(post !== `${product.nameRequest}\n` ? post : `${product.nameRequest}\nНе было изменений цены.`)
+				.catch(error => console.log(error));
 		});
 	} catch (error) {
 		console.log(error);
