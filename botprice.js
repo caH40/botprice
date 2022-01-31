28d292f23240b5d8bc0ca75861107363532d290f
caH40 <kmvsancho@gmail.com>
2022-01-31 09:28:16 +0300

    add aliexpress.ru

--- botprice.js
+++ botprice.js
@@ -48,9 +48,6 @@ bot.help(async (ctx) => {
 });
 
 // сцены
-bot.command('/description', async (ctx) => {
-	await ctx.reply(text.description, htmlDisPrev).catch((error) => console.log(error));
-});
 bot.command('/new', async (ctx) => {
 	await ctx.scene.enter('super-wizard').catch((error) => console.log(error));
 });
